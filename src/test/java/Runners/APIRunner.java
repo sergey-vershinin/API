8c26221524e1b57f78ffb6152f6c005ef5dfb643
iamdez99 <diegojuarezbusiness@gmail.com>
2021-07-24 14:13:51 -0400

    Last Rest Assured class code changes

--- src/test/java/Runners/APIRunner.java
+++ src/test/java/Runners/APIRunner.java
@@ -12,7 +12,7 @@ import org.junit.runner.RunWith;
         glue = "APISteps",
         //if we set dry run to true, it will quickly scan all gherkin steps are implemented or not
         // if it is true, then no actual execution happens
-        dryRun = true,
+        dryRun = false,
         //it means that the console output for the cucumber test is easily readable
         //it will remove unreadable character
         monochrome = true,
@@ -23,7 +23,7 @@ import org.junit.runner.RunWith;
         //tags will identify the scenarios based on the tags we will provide such as @smoke, @regression etc
         // we can add multiple tags in the runner class to execute scenarios belong to different tags such
         //as and or ,
-        tags = "@Random",
+        tags = "@APIWorkflow",
         plugin = {"pretty", "html:target/cucumber.html", "json:target/cucumber.json", "rerun:target/failed.txt"}
 )
 
